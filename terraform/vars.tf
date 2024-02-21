df8d97149c5b3aa529e4ef95a80991af60e47e4d
Mika Rinne <mika.rinne@oracle.com>
2024-02-21 16:45:40 +0200

    repo create

---
+++ terraform/vars.tf
@@ -0,0 +1,49 @@
+variable "region" {
+  type    = string
+  default = "REGION"
+  #default = "eu-amsterdam-1"
+}
+
+variable "compartment_id" {
+  type    = string
+  default = "ocid1.compartment.oc1..aaaa...whnznhmvgiqdatqgq"
+}
+
+variable "use_always_free" {
+  default = false
+}
+
+variable "tags" {
+  type    = string
+  default = "Created by Azure DevOps"
+}
+
+variable "demo_image_source_ocid" {
+  type    = string
+  default = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaahrz23nupvnphrewb35pwvn35bgcq5osths6iloszojm26slb2v4q"
+}
+
+variable "demo_shape" {
+  type    = string
+  default = "VM.Standard.E4.Flex"
+}
+
+variable "demo_shape_mem" {
+  type    = string
+  default = "8"
+}
+
+variable "demo_shape_ocpus" {
+  type    = string
+  default = "1"
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = ""
+}
+
+variable "demo_atp_admin_password" {
+  type    = string
+  default = "WelcomeFolks123#!"
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">=4.93.0"
+    }
+  }
+  backend "http" {
+    address = "https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/MqM0Iu1E6DXSYV...DDdltzepuSMWY8o/n/frsxwtjslf35/b/tf-state/o/terraform.tfstate"
+    update_method = "PUT"
+  }
+}
+
+provider "oci" {
+   auth = "InstancePrincipal"
+   region = "eu-amsterdam-1"
+}
